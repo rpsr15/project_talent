ee315869b825b02465d95b9ba1cae6c9391fa423
ravi rathore <ravirathore15@icloud.com>
2020-03-06 16:25:20 +1100

    added filters to the manage job

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -43,11 +43,11 @@ export default class ManageJob extends React.Component {
         this.setState({ loaderData });//comment this
 
         //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
-        
+        this.loadData(() => { console.log("load dta"); }
+        )
+
         //console.log(this.state.loaderData)
+
     }
 
     componentDidMount() {
@@ -55,9 +55,27 @@ export default class ManageJob extends React.Component {
     };
 
     loadData(callback) {
-        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        console.log("loading data");
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs?showActive=true&showClosed=true&showExpired=true&showUnexpired=true';
         var cookies = Cookies.get('talentAuthToken');
        // your ajax call and other logic goes here
+
+
+       
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                console.log("Here is the response!");
+                console.log(res);
+            }.bind(this)
+        });
     }
 
     loadNewData(data) {
@@ -77,8 +95,33 @@ export default class ManageJob extends React.Component {
     render() {
         return (
             <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
+                <div className="ui container">
+                    <h1>List of jobs </h1>
+                    <div className="row"><Icon name='filter'></Icon>
+                        <p style={{ display: "inline" }}>Filter: </p>
+                        <Dropdown text='Choose Filter'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item text='Active' />
+                                <Dropdown.Item text='Closed' />
+                                <Dropdown.Item text='Expired' />
+                                <Dropdown.Item text='Unexpired' />
+                                
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Icon name="calendar"> </Icon>
+                        <p style={{ display: "inline" }}>Sort by date: </p>
+                        <Dropdown text='Newest First'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item text='Newest First' />
+                                <Dropdown.Item text='Oldest First' />
+
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        </div>
+                    
+                </div>
             </BodyWrapper>
+
         )
     }
 }
